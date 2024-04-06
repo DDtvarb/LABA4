2f6f5a34f094e7ed9f504d315679fbba0d0a6ad0
DDtvarb <153864225+DDtvarb@users.noreply.github.com>
2024-04-06 13:19:17 +0300

    Add files via upload

---
+++ lab4.3.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+using namespace std;
+
+double F(double x, int n) {
+	if (n == 0) {
+		return 1;
+	}
+	else if (n > 0 && n % 2 == 0) {
+		double temp = F(x, n / 2);
+		return temp * temp;
+	}
+	else if (n > 0 && n % 2 != 0) {
+		return x * F(x, n - 1);
+	}
+	else {
+		return 1 / F(x, -n);
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "Ru");
+	double x;
+	cout << "Введите значение X " << endl;
+	cin >> x;
+	if (cin.fail()) {
+		cout << "Введите корректное значение X" << endl << endl;
+		exit(0);
+	}
+	
+	int n;
+	cout << "Введите значение степени " << endl;
+	cin >> n;
+	if (cin.fail()) {
+		cout << "Введите корректное значение степени" << endl << endl;
+		exit(0);
+	}
+
+	double result = F(x, n);
+	cout << x << " ^ " << n << " = " << result << endl;
+	
+
+	
+}
