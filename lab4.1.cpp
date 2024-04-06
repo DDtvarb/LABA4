6df5038dd1ebc260eb403b4f46d2f90c28400885
DDtvarb <153864225+DDtvarb@users.noreply.github.com>
2024-04-06 13:20:20 +0300

    Add files via upload

---
+++ lab4.1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int sumrange(int a, int b) {
+	if (a > b) {
+		return 0;
+	}
+
+	int sum = 0;
+	for (int i = a; i <= b; i++) {
+		sum += i;
+	}
+
+	return sum;
+}
+
+int main() {
+	setlocale(LC_ALL, "Ru");
+	
+	int a, b, c;
+	cout << "Введите A,B,C ";
+	cin >> a >> b >> c;
+	if (cin.fail()) {
+		cout << "Введите корректное значение" << endl << endl;
+		exit(0);
+	}
+
+	int sum1 = sumrange(a, b);
+	int sum2 = sumrange(b, c);
+
+	cout << "Cумма A и B: " << sum1 << endl;
+	cout << "Сумма B и С: " << sum2 << endl;
+
+	
+}
---
+++ lab4.2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+void sdvig(int& a, int& b, int& c) {
+	int temp = c;
+	c = b;
+	b = a;
+	a = temp;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a1, b1, c1, a2, b2, c2;
+	cout << "Введите a1, b1, c1, a2, b2, c2";
+	cin >> a1 >> b1 >> c1 >> a2 >> b2 >> c2;
+	if (cin.fail()) {
+		cout << "Введите корректное значение" << endl << endl;
+		exit(0);
+	}
+
+
+	sdvig(a1, b1, c1);
+	sdvig(a2, b2, c2);
+
+	cout << "1ый набор после циклического сдвига: " << a1 << " " << b1 << " " << c1 << endl;
+	cout << "2ой набор после циклического сдвига: " << a2 << " " << b2 << " " << c2 << endl;
+
+	return 0;
+}
+
